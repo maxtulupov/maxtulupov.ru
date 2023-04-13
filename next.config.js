64a9c07f0546a5edbe074c6083c855bf74ff5032
itulupov1 <itulupov1@yandex.ru>
2023-04-14 00:58:11 +0700

    Удаление лишнего из next congif

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   // reactStrictMode: true,
   poweredByHeader: false,
-  // optimizeFonts: false,
   env: {
     APP_URL: process.env.REACT_APP_URL,
     APP_ENV: process.env.REACT_APP_ENV,
